d37aaea3e9835f089a1f4511abdd4fb5cc98bc27
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-02-08 00:02:16 +0100

    migration: Add ConnectEntities

--- TrainTime_Database/Migrations/20260207225852_ConnectEntities.Designer.cs
+++ TrainTime_Database/Migrations/20260207225852_ConnectEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrainTime_Database;
 
@@ -11,9 +12,11 @@ using TrainTime_Database;
 namespace TrainTime_Database.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260207225852_ConnectEntities")]
+    partial class ConnectEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

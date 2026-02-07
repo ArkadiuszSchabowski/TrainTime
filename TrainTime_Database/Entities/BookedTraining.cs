e888aad0f5e6143f46411d93ae13fcce68035ca5
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-02-07 19:01:59 +0100

    feat: create database project with entities

---
+++ TrainTime_Database/Entities/BookedTraining.cs
@@ -0,0 +1,14 @@
+﻿namespace TrainTime_Database.Entities
+{
+    public class BookedTraining
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public int TrainerId { get; set; }
+        public Trainer? Trainer { get; set; }
+        public int GymId { get; set; }
+        public Gym? Gym { get; set; }
+        public DateTime Date { get; set; } = new();
+    }
+}
---
+++ TrainTime_Database/Entities/Trainer.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainTime_Database.Entities
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public  string? Description { get; set; }
+        public List<TrainerWorkingPeriod> WorkingPeriods { get; set; } = new();
+    }
+}
